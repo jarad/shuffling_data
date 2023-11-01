7aaf3ebdc5dd8dee005e4200179279cc8537d211
Jarad Niemi <niemi@iastate.edu>
2023-11-01 10:13:47 -0500

    Remove print-out from test of shuffling data

--- code/tests/test-shuffling.R
+++ code/tests/test-shuffling.R
@@ -13,7 +13,6 @@ for (i in seq_along(files)) {
   )
   
   for (c in 1:ncol(d)) {
-    cat(c)
     test_that(paste0(file, " column ", c," elements are 1:52"),
               expect_equal(1:52, sort(as.integer(d[,c]))))
   }
